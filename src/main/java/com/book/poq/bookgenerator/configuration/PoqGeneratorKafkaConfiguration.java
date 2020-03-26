32fb86537f630e411ee96e547a673d6024b24afb
steampunkdog <nekit2101.user@gmail.com>
2020-03-26 11:40:12 +0200

    BookGeneratorPoq patch 0.1 properties for Kafka

--- src/main/java/com/book/poq/bookgenerator/configuration/PoqGeneratorKafkaConfiguration.java
+++ src/main/java/com/book/poq/bookgenerator/configuration/PoqGeneratorKafkaConfiguration.java
@@ -1,19 +1,21 @@
 package com.book.poq.bookgenerator.configuration;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
+@EnableConfigurationProperties(PoqBookTopicProperties.class)
 public class PoqGeneratorKafkaConfiguration {
 
     @Bean
-    public NewTopic bookTopic(){
+    public NewTopic bookTopic(PoqBookTopicProperties properties){
         return TopicBuilder
-                .name("BookTopic")
-                .partitions(2)
-                .replicas((short)1)
+                .name(properties.getName())
+                .partitions(properties.getPartitions())
+                .replicas(properties.getReplicas())
                 .build();
     }
 }
